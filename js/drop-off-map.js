07e4bc24cc7358fea38f4f2ee95d4eee863efb95
Leonardo Alberto Souza <leobetosouza@gmail.com>
2013-03-11 07:55:33 -0300

    drop angular

--- js/drop-off-map.js
+++ js/drop-off-map.js
@@ -1,33 +1,71 @@
-angular.module( 'drop-off-map', [ 'ui' ] );
+var dropOffApp = {};
 
-function MapCtrl( $scope ) {
+(function( window, app ) {
 
-	var latLng = new google.maps.LatLng( pos.coords.latitude, pos.coords.longitude );
+	'use strict';
 
-	$scope.mapOptions = {
-		center: latLng,
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+	var document = window.document;
+
+	app.pins = {
+		recycle : '../img/recycle.png'
 	};
 
-	$scope.onMapIdle = function() {
+	app.markers = [];
 
-		var marker = new google.maps.Marker({
-			icon: '../img/recycle.png',
-			map: $scope.myMap,
-			position: latLng,
-			name: 'Center',
-			title: 'Você está aqui!'
+	app.init = function ( pos ) {
+
+		var mapCanvas = document.getElementById( 'map-canvas' );
+		var latLng = new google.maps.LatLng( pos.coords.latitude, pos.coords.longitude );
+		var mapOptions = {
+			center: latLng,
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		app.map = new google.maps.Map( mapCanvas, mapOptions );
+
+		var center = new google.maps.Marker({
+			position: latLng, 
+			map: app.map,
+			scrollwheel: true,
+			streetViewControl: true,
+			labels: true,
+			pin: app.pins.recycle,
+			title: "Sua posição"
 		});
 
-		$scope.myMarkers = [marker, ];
+		app.markers.push( center );
 
 	};
 
-	$scope.markerClicked = function( m ) {
+	function requestLocation( success ) {
 
-		window.alert( this.center );
+		navigator.geolocation.getCurrentPosition( success, function() {
 
-	};
+			alert( 'Você deve compartilhar a sua pocisão conosco...');
 
-}
+			requestLocation( success );
+
+		});
+
+	}
+
+	document.addEventListener( "readystatechange", function() {
+
+		if (document.readyState === "interactive") {
+
+			if (navigator.geolocation) {
+
+				requestLocation( app.init );
+
+			} else {
+
+				alert( 'Seu navegador não suporta' );
+
+			}
+
+		}
+
+	}, false );
+
+}( window, dropOffApp ));
