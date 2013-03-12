93a4890de4ac578d1c2dfb403bb37c50ded9d7a8
Leonardo Alberto Souza <leobetosouza@gmail.com>
2013-03-12 19:18:26 -0300

    funciona se o usuario nao entregar location

--- js/drop-off-map.js
+++ js/drop-off-map.js
@@ -16,54 +16,58 @@ var dropOffApp = {};
 
 	app.init = function ( pos ) {
 
-		var mapCanvas = document.getElementById( 'map-canvas' );
-		var center = new google.maps.LatLng( pos.coords.latitude, pos.coords.longitude );
-		var mapOptions = {
-			center: center,
-			zoom: 12,
-			scrollwheel: true,
-			streetViewControl: true,
-			labels: true,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-
-		app.map = new google.maps.Map( mapCanvas, mapOptions );
-
-		var user = new google.maps.Marker({
-			position: center, 
-			map: app.map,
-			title: "Sua posição"
-		});
-
-		app.points.push( center );
-
 		getJSON( 'data/niteroi-rj.json', function( data ) {
+			
+			var city = JSON.parse( data );
 
-			var points = JSON.parse( data );
+			var mapCanvas = document.getElementById( 'map-canvas' );
 
-			points.forEach(function( pt ){
+			var center = ( pos ) ? new google.maps.LatLng( pos.coords.latitude, pos.coords.longitude ) :
+														 new google.maps.LatLng( city.center.lat, city.center.long );
+
+			var mapOptions = {
+				center: center,
+				zoom: 12,
+				scrollwheel: true,
+				streetViewControl: true,
+				labels: true,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			app.map = new google.maps.Map( mapCanvas, mapOptions );
+
+			if ( pos ) {
+
+				app.points.push( new google.maps.Marker({
+					position: center, 
+					map: app.map,
+					title: "Sua posição"
+				}));
+
+			}
+
+			city.points.forEach(function( pt ){
 
 				var pos = new google.maps.LatLng( pt.lat, pt.long );
-				
+
 				var marker = new google.maps.Marker({
 					position: pos, 
 					map: app.map,
 					icon: app.pins.recycle,
 					title: pt.name,
 					//address : pt.address + ' ' + app.cities[ 0 ],
-					//tels : pt.tels.slice( '|' )
+					//tels : pt.tels.slice( '|' ),
+					infoWindow : new google.maps.InfoWindow({
+						content: pt.name + '<br>' + pt.address + ' ' + app.cities[ 0 ] + '<br>' + pt.tels.slice( '|' ),
+						position: pos
+					})
 				});
-
-				var infoWindow = new google.maps.InfoWindow({
-					content: pt.name + '\n' + pt.address + ' ' + app.cities[ 0 ] + '\n' + pt.tels.slice( '|' ),
-		      position: pos
-		    });
 
 				google.maps.event.addListener( marker, 'click', function( e ) {
 					
-		      infoWindow.open( app.map );
+					marker.infoWindow.open( marker.map );
 
-			  });
+				});
 
 				app.points.push( marker );
 
@@ -73,13 +77,17 @@ var dropOffApp = {};
 
 	};
 
-	function requestLocation( success ) {
+	function requestUserLocation( success, failMsg ) {
 
 		navigator.geolocation.getCurrentPosition( success, function() {
 
-			alert( 'Você deve compartilhar a sua posição conosco...');
+			if ( confirm( failMsg ) ) {
 
-			requestLocation( success );
+				requestUserLocation( success, failMsg );
+
+			}
+
+			success();
 
 		});
 
@@ -93,9 +101,9 @@ var dropOffApp = {};
 		xhr.send( null );
 		xhr.onreadystatechange = function() {
 
-				if ( this.status == 200 && this.readyState == 4 ) {
+				if ( this.status === 200 && this.readyState === 4 ) {
 
-						success(this.responseText);
+						success( this.responseText );
 
 				}
 
@@ -105,15 +113,15 @@ var dropOffApp = {};
 
 	document.addEventListener( "readystatechange", function() {
 
-		if (document.readyState === "interactive") {
+		if ( document.readyState === "interactive" ) {
 
-			if (navigator.geolocation) {
+			if ( navigator.geolocation ) {
 
-				requestLocation( app.init );
+				requestUserLocation( app.init, 'Você deve compartilhar a sua posição conosco para podermos identificar os pontos de coleta próximos à você' );
 
 			} else {
 
-				alert( 'Seu navegador não suporta' );
+				app.init();
 
 			}
 
